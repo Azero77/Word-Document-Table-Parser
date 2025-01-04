9145b0512edd00892a0a9e2477cc2cac2c822745
Azero 77 <zaggar.azero.m@gmail.com>
2025-01-05 01:12:24 +0300

    Added Lazy Question Parser

--- ParserConsole/WordDocumentTableParserProject/QuestionParser.cs
+++ ParserConsole/WordDocumentTableParserProject/QuestionParser.cs
@@ -5,6 +5,7 @@ using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,69 +14,90 @@ namespace WordDocumentTableParserProject
     /// <summary>
     /// Take the question from the table and lazily load it
     /// </summary>
-    public class QuestionParser
+    public class QuestionParser : IDisposable
     {
         private readonly WordprocessingDocument _document;
         public QuestionParser(WordprocessingDocument document)
         {
             _document = document;
         }
-        public List<Question> ParseQuestions(string filePath)
-        {
-            List<Question> questions = new();
-
-            using (_document)
-            {
-                var body = _document.MainDocumentPart.Document.Body;
-                var table = body?.Elements<Table>().Single();
-                var rows = table?.Elements<TableRow>();
-
-                    // Process rows in pairs (odd row + even row)
-                for (int i = 0; i < 12; i += 2)
-                {
-                    var oddRow = rows.ElementAt(i);
-                    var evenRow = i + 1 < rows.Count() ? rows.ElementAt(i + 1) : null;
-
-                    if (oddRow != null && evenRow != null)
-                    {
-                        var oddCells = oddRow.Elements<TableCell>();
-                        var evenCells = evenRow.Elements<TableCell>();
-
-                        if (oddCells.Count() == 3 && evenCells.Count() == 3)
-                        {
-                            Question question = new()
-                            {
-                                QuestionText = GetTextFromCell(oddCells.ElementAt(1)),
-                                QuestionAnswerIndex = (GetTextFromCell(evenCells.ElementAt(2)))[0] - 'A'
-                            };
-
-                            // Extract choices from the second column of the even row
-                            var choicesParagraphs = evenCells.ElementAt(1).Elements<Paragraph>();
-                            foreach (var paragraph in choicesParagraphs)
-                            {
-                                question.Choices.Add(paragraph.InnerText.Trim());
-                            }
-
-                            questions.Add(question);
-                        }
-                    }
-                    }
-                return questions;
-            }
-            string GetTextFromCell(TableCell cell)
-            {
-                return cell.InnerText.Trim();
-            }
-        }
-
         /// <summary>
         /// Generator for questions to lazy load every question
         /// </summary>
         /// <returns></returns>
         public IEnumerable<Question> GetQuestion()
         {
-            throw new NotImplementedException();
+            TableRow evenRow, oddRow;
+            (evenRow, oddRow) = AssignRows();
+            if (oddRow != null && evenRow != null)
+            {
+                Question question = ProcessQuestion(evenRow, oddRow);
+                yield return question;
+            }
+            else
+                yield break;
         }
 
+        private static Question ProcessQuestion(TableRow evenRow, TableRow oddRow)
+        {
+            var oddCells = oddRow.Elements<TableCell>();
+            var evenCells = evenRow.Elements<TableCell>();
+
+            if (oddCells.Count() == 3 && evenCells.Count() == 3)
+            {
+                Question question = new()
+                {
+                    QuestionText = GetTextFromCell(oddCells.ElementAt(1)),
+                    QuestionAnswerIndex = (GetTextFromCell(evenCells.ElementAt(2)))[0] - 'A'
+                };
+
+                // Extract choices from the second column of the even row
+                var choicesParagraphs = evenCells.ElementAt(1).Elements<Paragraph>();
+                foreach (var paragraph in choicesParagraphs)
+                {
+                    question.Choices.Add(paragraph.InnerText.Trim());
+                }
+                return question;
+
+            }
+            throw new InvalidDataException("Must Have Three columns for each row");
+
+            string GetTextFromCell(TableCell cell)
+            {
+                return cell.InnerText.Trim();
+            }
+        }
+
+        private (TableRow even,TableRow odd) AssignRows()
+        {
+            TableRow even,odd;
+            IEnumerator<TableRow> enumerator = RowGenerator().GetEnumerator();
+            odd = enumerator.Current;
+            enumerator.MoveNext();
+            even = enumerator.Current;
+            return (even, odd);
+        }
+
+        /// <summary>
+        /// Lazily load every table row 
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<TableRow> RowGenerator()
+        {
+            IEnumerable<Table> tables = _document?.MainDocumentPart?.Document?.Body?.Elements<Table>() ?? throw new InvalidDataException("Invalid Document");
+            if (tables.Count() != 1)
+                throw new InvalidDataException("Document Contains zero or More than one table please select the current table by index or name");
+            Table table = tables.Single();
+            IEnumerable<TableRow> rows = table.Elements<TableRow>();
+            foreach (var row in rows)
+            {
+                yield return row;
+            }
+        }
+
+        public void Dispose()
+        {
+            _document?.Dispose();
+        }
     }
 }
