964eceef86dec31c69c6822f284bc0a923d649ba
Azero 77 <zaggar.azero.m@gmail.com>
2025-03-14 21:14:27 +0300

    Question Class made

--- ParserConsole/WordDocumentTableParserProject/Models/Question.cs
+++ ParserConsole/WordDocumentTableParserProject/Models/Question.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WordDocumentTableParserProject
 {
     public class Question
     {
-        public List<string> Choices { get; set; } = new List<string>();
-        public required string QuestionText { get; set; }
+        public List<List<QuestionSegment>> QuestionChoices { get; set; } = null!; //every choice is a list of Question segment
+        public required List<QuestionSegment> QuestionText { get; set; }
         public string Answer { get; set; } = string.Empty;
     }
 
+    public class QuestionSegment
+    {
+        public List<QuestionSentence> Sentences = null!;
+    }
 
+    public class QuestionSentence
+    {
+        public string Text { get; set; } = string.Empty;
+        public QuestionSentenceType QuestionSentenceType { get; set; } = QuestionSentenceType.SimpleText;
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? AltText { get; set; } = null;
+
+    }
+
+    public enum QuestionSentenceType
+    {
+        SimpleText,
+        Equation,
+        ImageUrl,
+        CodeBlock,
+        Table
+    }
 }
